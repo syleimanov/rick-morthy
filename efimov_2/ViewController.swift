e4c95e27f8db0721d31363c1bc84fefd282c80d3
VLADIMIR SYLEIMANOV <bannabels@gmail.com>
2021-01-31 20:21:17 +0300

    Initial Commit

---
+++ efimov_2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  efimov_2
+//
+//  Created by Vladimir Syleimanov on 31.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
